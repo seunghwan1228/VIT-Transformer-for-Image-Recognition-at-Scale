0615682aa8f3fbf8e59c5dee1b617167c7a92a61
Han <50775491+seunghwan1228@users.noreply.github.com>
2020-10-29 18:06:27 +0900

    Working: Building DataLoader

---
+++ datahandler/load_data.py
@@ -0,0 +1,113 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
+
+
+class DataLoader:
+    def __init__(self, data_name: str, data_split: tuple, target_size: int, batch_size: int, normalize: str, img_patch_size=16):
+        '''
+        data_split should follow the order: train, validation, test
+        '''
+        self.data_name = data_name
+        self.target_size = target_size
+        self.batch_size = batch_size
+        self.normalize = normalize
+        self.img_patch_size = img_patch_size
+        self.data_split = data_split # tuple
+        assert self.normalize in ['standard', 'minmax'], f'Requires to be Normalize <standard or minmax> nor {self.normalize}'
+
+
+    def _download_data(self, return_info=False):
+        examples, info = tfds.load(self.data_name, as_supervised=True, with_info=True)
+        examples_length = len(examples)
+        print(f'The Data has {examples_length}')
+
+        if return_info:
+            return examples, info
+        else:
+            return examples
+
+
+    def _split_data(self, examples):
+        if len(self.data_split) == 1:
+            return examples[self.data_split[0]]
+
+        elif len(self.data_split) == 2:
+            return examples[self.data_split[0]], examples[self.data_split[1]]
+
+        elif len(self.data_split) == 3:
+            return examples[self.data_split[0]], examples[self.data_split[1]], examples[self.data_split[2]]
+
+
+    def _resize_img(self, img, label, size):
+        img = tf.cast(img, tf.float32)
+        img = tf.image.resize(img, size=(size, size), method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        return (img, label)
+
+
+    def _standard_normalize(self, img, label):
+        img = (img/127.5) - 1.
+        return (img, label)
+
+
+    def _minmax_normalize(self, img, label):
+        img = img/255.
+        return (img, label)
+
+
+    def _patch_img(self, img, patch_size=16):
+        img = tf.expand_dims(img, axis=0)
+        img_size = img.shape
+        img_height = img_size[1]
+        img_width = img_size[2]
+
+        def box_normalize(x):
+            return x/img_height
+
+        patchs_result = []
+        for h in range(0, img_height, patch_size):
+            for w in range(0, img_width, patch_size):
+
+                normalize_box = [box_normalize(h), box_normalize(w), box_normalize(h + patch_size),
+                                 box_normalize(w + patch_size)]  # box = [y, x, y1, x1]
+
+                each_patch = tf.image.crop_and_resize(img, [normalize_box], box_indices=[0], method='nearest',
+                                                      crop_size=(patch_size, patch_size))
+                patchs_result.append(each_patch)
+        return tf.concat(patchs_result, axis=0)
+
+    #TODO: Requires to Check << tf.image.extract_patches >>
+    def _fast_patch_img(self, img, patch_size):
+        tf.image.extract_patches(img,
+                                 sizes=[1, patch_size, patch_size, 1],
+                                 strides=[1, patch_size, patch_size, 1],
+                                 rates=[1,1,1,1],
+                                 padding='VALID')
+
+
+    def _tf_patch_image(self, img, label, patch_size=16):
+        x = tf.py_function(self._patch_img, inp=[img, patch_size], Tout=tf.float32)
+        return (x, label)
+
+
+    def _transform_img_to_seq(self, img, label, patch_size=16):
+        B = int(self.batch_size)
+        H = int(self.target_size)
+        W = int(self.target_size)
+        C = 3
+        P = int(patch_size)
+        N = int((H * W) / (P ** 2))
+
+        img_transform = tf.reshape(img, shape=(N, (P ** 2 * C)))
+        return (img_transform, label)
+
+    #TODO DataCreation Call
+    def __call__(self):
+        pass
+
+
+
+
+
+if __name__ == '__main__':
+    data_loader = DataLoader('cifar10', ('train', 'test'), 224, 14, 'standard')
